833fd7a8dc2a00b2cccf1c35a3bcaeb17a8ab816
SerhiiRud <carmagsales@gmail.com>
2023-04-14 11:25:23 +0300

    update

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,7 +4,7 @@ import css from './Statistics.module.css';
 export const Statistics = ({ stats, title }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(item => (
           <li key={item.id} className={css.item}>
